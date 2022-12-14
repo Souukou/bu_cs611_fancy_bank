6f872342f809a7929be1005d349913718fdf37ef
Yuhang Song <i@syh.one>
2022-12-14 00:56:40 -0500

    Add username to User

--- src/main/java/fancybank/user/User.java
+++ src/main/java/fancybank/user/User.java
@@ -2,6 +2,7 @@ package fancybank.user;
 
 public abstract class User {
     private UID UID;
+    private Username username;
     private Name name;
     private Address address;
     private Email email;
@@ -9,21 +10,24 @@ public abstract class User {
 
     public User() {
         this.UID = new UID();
+        this.username = new Username();
         this.name = new Name();
         this.address = new Address();
         this.email = new Email();
     }
 
-    public User(UID UID, Name name, Address address, Email email, Password password) {
+    public User(UID UID, Username username, Name name, Address address, Email email, Password password) {
         this.UID = UID;
+        this.username = username;
         this.name = name;
         this.address = address;
         this.email = email;
         this.password = password;
     }
 
-    public User(int uid, String firstName, String middleName, String lastName, String street, String city, String state, String zip, String country, String email, String password) {
+    public User(int uid, String username, String firstName, String middleName, String lastName, String street, String city, String state, String zip, String country, String email, String password) {
         this.UID = new UID(uid);
+        this.username = new Username(username);
         this.name = new Name(firstName, middleName, lastName);
         this.address = new Address(street, city, state, zip, country);
         this.email = new Email(email);
@@ -36,6 +40,14 @@ public abstract class User {
 
     public void setUID(UID UID) {
         this.UID = UID;
+    }
+
+    public Username getUsername() {
+        return username;
+    }
+
+    public void setUsername(Username username) {
+        this.username = username;
     }
 
     public Name getName() {
@@ -71,7 +83,7 @@ public abstract class User {
     }
 
     public String toString() {
-        return "UID: " + UID + " Name: " + name + " Address: " + address + " Email: " + email;
+        return "UID: " + UID + "Username: " + username + " Name: " + name + " Address: " + address + " Email: " + email;
     }
 
 }
