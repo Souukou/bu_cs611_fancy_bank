41f2075d5b2e9e1ad60e9abdac24de50db91b958
Yuhang Song <i@syh.one>
2022-12-15 13:57:51 -0500

    Add init manager

--- src/test/java/fancybank/bank/BankTest.java
+++ src/test/java/fancybank/bank/BankTest.java
@@ -11,18 +11,18 @@ public class BankTest {
     public void TestCostomerLoginAndRegister() {
         Data.getInstance().getManagers().size();
 
-        Bank.getInstance().ManagerRegister("mmmmc", "Michael", "", "Jackson", "1601 Queens Road, Hollywood Hills",
-                "Los Olivos", "California", "93441", "US", "mike@gmail.com", "qwert123");
+        Bank.getInstance().ManagerRegister("admin", "Michael", "", "Jackson", "1601 Queens Road, Hollywood Hills",
+                "Los Olivos", "California", "93441", "US", "mike@gmail.com", "12345678");
 
         Assertions.assertTrue(Data.getInstance().getManagers().size() >= 1);
         // wrong pass
-        Manager m = Bank.getInstance().ManagerLogin("mmmmc", "qwert");
+        Manager m = Bank.getInstance().ManagerLogin("admin", "qwert");
         Assertions.assertNull(m);
         // wrong username
-        m = Bank.getInstance().ManagerLogin("mmmmcc", "qwert123");
+        m = Bank.getInstance().ManagerLogin("adminx", "12345678");
         Assertions.assertNull(m);
         // right
-        m = Bank.getInstance().ManagerLogin("mmmmc", "qwert123");
+        m = Bank.getInstance().ManagerLogin("admin", "12345678");
         Assertions.assertNotNull(m);
         Assertions.assertEquals("Michael", m.getName().getFirstName());
         Assertions.assertEquals("Jackson", m.getName().getLastName());
